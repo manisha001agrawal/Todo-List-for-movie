4408b5d64d796f618277c890c0968a23d80b3d00
manisha001agrawal <67043518+manisha001agrawal@users.noreply.github.com>
2021-08-20 11:42:41 +0530

    Create database_connection.dart

---
+++ repositories/database_connection.dart
@@ -0,0 +1,15 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseConnection{
+  setDatabase() async{
+    var directory = await getApplicationDocumentsDirectory();
+    var path = join(directory.path,'db_todolist');
+    var database = await openDatabase(path, version: 1, onCreate: _onCreateDatabase);
+    return database;
+  }
+  _onCreateDatabase(Database database,int version) async{
+    await database.execute('create TABLE categories (id INTEGER PRIMARY KEY, name TEXT, description TEXT)');
+  }
+}
